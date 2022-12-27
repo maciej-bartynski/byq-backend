17f0b5e8eb6a8c802d82914939ed29a4ef624f0d
maciej.bartynski <maciej.bartynski@amsterdamstandard.com>
2022-12-27 10:39:29 +0100

    modify run.shj

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 3000
 git checkout master && git pull && MARKER=staging node ./envs/create-env.js && npm install && tmux && node ./index.js
